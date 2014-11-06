c39cd5e00bb41411af1d85292d0ae48cd74b53a5
Kenneth Mwiti <Mwiri@Kenneths-MacBook-Air.local>
2014-11-06 11:05:41 +0300

    Added label for name text fielld text.

--- Swift Training/ViewController.swift
+++ Swift Training/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var enterNameTextField: UITextField!
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
@@ -34,6 +35,12 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        nameLabel.hidden = false
+        enterNameTextField.text = ""
+        
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
